373194371665ab4454d577f24f1bcc8f5d7ee20b
francescobilla <francesco.billa2004@gmail.com>
2021-03-16 13:14:09 +0100

    aggiunta modifiche

--- TestTariffe/TestMetodiTariffe.cs
+++ TestTariffe/TestMetodiTariffe.cs
@@ -6,11 +6,12 @@ namespace TestTariffe
     [TestClass]
     public class TestMetodiTariffe
     {
-        [TestMethod]
-        public void TestTariffaConveniente()
+        [DataTestMethod]
+        [DataRow(60, "si consiglia un mese")]
+        [DataRow(170, "si consiglia 3 mesi")]
+        [DataRow(360, "si consiglia 12 mesi")]
+        public void TestTariffaConveniente(int giorno, string risultato_aspettato)
         {
-            int giorno = 60;
-            string risultato_aspettato = "si consiglia un mese";
             string risp = Gestione.TariffaConveniente(giorno);
             Assert.AreEqual(risultato_aspettato, risp);
         }
